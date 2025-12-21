d91dcc988d00d1f585172318d80dd548926d7a51
Tanguy Cadoux <tanguycadoux04@gmail.com>
2025-12-21 14:17:20 +0100

    Suppression de select2

--- app_gestion/app_gestion/settings.py
+++ app_gestion/app_gestion/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# NOTES_DIR = BASE_DIR / "notes_quarto_files"
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -38,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_select2',
     'note_taking',
     'todolist',
 ]
