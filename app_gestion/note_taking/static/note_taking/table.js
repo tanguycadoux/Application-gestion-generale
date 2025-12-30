a32449d7a0f83c9ce210d389faec1e27a66994b5
Tanguy Cadoux <tanguycadoux04@gmail.com>
2025-12-30 11:39:32 +0100

    Clic sur une colonne sans href ouvre le popover

--- app_gestion/note_taking/static/note_taking/table.js
+++ app_gestion/note_taking/static/note_taking/table.js
@@ -1,27 +1,44 @@
-async function tableColumnOptions(element) {
+async function tableColumnOptions(columnButton) {
     var pop_html = `
     <p>Test</p>
     `
-    const { pop, ready, close } = createPopoverBase(element, pop_html, 
+    const { pop, ready, close } = createPopoverBase(columnButton, pop_html, 
         alignment={ "top": "bottom", "left": "left" }
     );
-
+    
     await ready;
+    pop.classList.add("table-column-options-popover");
+    columnButton.classList.add("table-column-head--active-popover");
 
     return new Promise((resolve) => {
-        close.then(() => resolve(false));
+        close.then(() => {
+            columnButton.classList.remove("table-column-head--active-popover");
+            resolve(false);
+        });
     });
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".table-column-head__more").forEach(button => {
+    document.querySelectorAll(".table-column-head__options").forEach(button => {
         button.addEventListener("click", event => {
             event.stopPropagation();
             event.preventDefault();
 
-            const columnButton = button.closest("a");
+            const columnButton = button.closest(".table-column-head");
 
             tableColumnOptions(columnButton);
         });
+    });
+
+    document.querySelectorAll(".table-column-head").forEach(button => {
+        if (button.href === undefined) {
+            button.addEventListener("click", event => {
+                event.stopPropagation();
+                event.preventDefault();
+
+                optionsButton = button.querySelector(".table-column-head__options");
+                optionsButton.click();
+            });
+        }
     });
 });
